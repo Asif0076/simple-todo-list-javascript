1d928ae15fd1b3d4d62e6d13165521926580d686
Asif Nawaz <assu.nawaz007@gmail.com>
2022-11-29 03:35:45 +0500

    added edit button

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ span.textContent = input.value;
 
             //create edit button
 const editBtn = document.createElement("button");
-editBtn.textContent = "edit";
+editBtn.textContent = "Edit";
 
             // create remove button
 const deleteBtn = document.createElement("button");
@@ -51,6 +51,20 @@ if(event.target.tagName === "BUTTON") {
     const ul = li.parentNode;
 if(button.textContent === "Delete" ) {
 ul.removeChild(li);
+}else if (button.textContent === "Edit") {
+const span = li.firstElementChild;
+const input = document.createElement("input");
+input.type = "text";
+input.value = span.textContent;
+li.insertBefore(input , span);
+li.removeChild(span);
+button.textContent = "Save";
+}else if (button.textContent === "Save") {
+const input = li.firstElementChild;
+const span = document.createElement("span");
+span.textContent = input.value;
+li.insertBefore(span , input);
+li.removeChild(input);
 }
 }
 });
